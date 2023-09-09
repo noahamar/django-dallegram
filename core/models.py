86cee270abd77ddcf7b74fdedc00037961dc43c2
Noah Amar <noahamar@gmail.com>
2023-09-08 21:07:15 -0400

    feat(app): Initial setup

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/')
+    caption = models.CharField(max_length=1000)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.caption
