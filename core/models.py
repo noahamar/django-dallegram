493301f97122f8c5eea11463724b37b36d9d6630
Noah Amar <noahamar@gmail.com>
2023-09-10 18:12:18 -0400

    feat(app): Implement openai request

--- core/models.py
+++ core/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# # Create your models here.
-# class Post(models.Model):
-#     image = models.ImageField(upload_to='images/')
-#     caption = models.CharField(max_length=1000)
-#     date_created = models.DateTimeField(auto_now_add=True)
+User = get_user_model()
 
-#     def __str__(self):
-#         return self.caption
+#  Profile model
+class Profile(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  display_name = models.CharField(max_length=100, blank=True)
+  profile_image = models.ImageField(upload_to='profile_images', default='profile_images/default.png')
+
+  def __str__(self):
+    return self.user.username
+
+#  Post model
+class Post(models.Model):
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+  image = models.ImageField(upload_to='post_images', blank=True)
+  caption = models.TextField(blank=True)
+  date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.caption
--- core/views.py
+++ core/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
+from .models import Profile, Post
+from dotenv import load_dotenv
+import openai, os, requests
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY', None)
+openai.api_key = openai_api_key
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("<h1>Dall•e•gram</h1>")
-    return render(request, 'index.html')
+    user = get_user_model().objects.get(username="noahamar")
+    profile = Profile.objects.get(user=user)
+    if openai_api_key is not None and request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        if prompt is None:
+            return redirect('/')
+        openai_response = openai.Image.create(
+            prompt=prompt,
+            size='256x256'
+        )
+        image_url = openai_response["data"][0]["url"]
+        image_response = requests.get(image_url)
+        image_file = ContentFile(image_response.content)
+        count = Post.objects.count() + 1
+        post = Post(profile=profile, caption=prompt)
+        post.image.save(f'post-{count}.jpg', image_file)
+        post.save()
+        return redirect('/')
+    posts = Post.objects.all().order_by('-date')
+    suggested_profiles = Profile.objects.exclude(user=user).order_by('?')[:4]
+    return render(request, 'index.html', {
+        "posts": posts, 
+        "suggested_profiles": suggested_profiles 
+    })
